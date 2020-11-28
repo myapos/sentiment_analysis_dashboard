70ad67dd36c1d44c58840568d51cd3b12a19d062
Myron <myron.apostolakis@onapp.com>
2020-11-29 00:36:44 +0200

    completed animation for page not found

--- src/pages/NoMatch/index.js
+++ src/pages/NoMatch/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { createUseStyles } from "react-jss";
+import classnames from "classnames";
+
+import { styles } from "./styles";
+import { commonStyles } from "../../common/styles";
+const useStyles = createUseStyles({ ...styles, ...commonStyles });
 
 function NoMatch(props) {
-  return <div>404</div>;
+  const classes = useStyles();
+  return (
+    <div className={classnames(classes.message, classes.center)}>
+      Page Not Found
+    </div>
+  );
 }
 
 export default NoMatch;
