1280723e112ded12a37af35a0418b56eeee32253
Myron <myron.apostolakis@onapp.com>
2020-12-02 23:51:00 +0200

    checked greek queries

--- src/api/fetchTweets.js
+++ src/api/fetchTweets.js
@@ -2,7 +2,9 @@ import customFetch from "../utils/customFetch";
 
 const fetchTweets = async (term) => {
   // const url = "http://localhost:8585/check";
-  const url = `http://localhost:8585/tweets?query=${term}`;
+  const url =
+    "http://localhost:8585/tweets?query=" +
+    window.encodeURIComponent(term).replace(/\s/g, "");
   const res = customFetch(url);
 
   return res;
