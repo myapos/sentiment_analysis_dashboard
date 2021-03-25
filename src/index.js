466c7279635dd70ce151d7ea4b0184a62a6d39c2
Myron Apostolakis <myron.apostolakis@sunlight.io>
2021-03-25 16:14:14 +0200

    test 4

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
 import "react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.css";
 
-console.log("test2");
+console.log("test3");
 
 ReactDOM.render(
   <React.StrictMode>
